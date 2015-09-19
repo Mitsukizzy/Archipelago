9d44d4e0b91a3d0f8360f3aa8ac160fa244e5e8e
Izzy Benavente <ibenaven@usc.edu>
2015-09-18 21:51:47 -0700

    Wrote some basic combo code. 4 Left attacks, 5 Right attacks.

--- Assets/__Scripts/Combat.cs
+++ Assets/__Scripts/Combat.cs
@@ -1,33 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
+using UnityEngine.UI;
 
 public class Combat : MonoBehaviour 
 {
-    float multClickTimer = 0.0f;
-    float delayBetweenMultClicks = 0.5f;
+    float clickTimer = 0.0f;
+    float prevClickTimer = 0.0f;
+    float fadeTimer = 0.0f;
+    float delayBetweenMultClicks = 0.75f;
     int multClickCountLeft = 0;
     int multClickCountRight = 0;
 
+    private GameObject m_Canvas;
+    private GameObject m_ComboTextObj;
+    private Text m_ComboText;
+
 	// Use this for initialization
 	void Start () {
-	
+        m_Canvas = GameObject.Find ( "Canvas" );
+        m_ComboTextObj = ( GameObject )Instantiate ( Resources.Load ( "ComboText" ) );
+        m_ComboTextObj.SetActive ( false );
+	    m_ComboText = m_ComboTextObj.GetComponent<Text>();
+        m_ComboText.rectTransform.parent = m_Canvas.transform;
+        m_ComboText.rectTransform.localPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         // TODO: Replace with Input Manager hooks
+        fadeTimer -= Time.deltaTime;
+        if ( fadeTimer < 0 )
+        {
+	        m_ComboTextObj.SetActive( false );
+	    }
+
         if ( Input.GetMouseButtonDown( 0 ) )
         {
-            multClickTimer = Time.time;
+            clickTimer = Time.time;
             LeftClickCombo();
-            Debug.Log( "Pressed left click." );
         }
 
         if ( Input.GetMouseButtonDown( 1 ) )
         {
-            multClickTimer = Time.time;
+            clickTimer = Time.time;
             RightClickCombo();
-            Debug.Log( "Pressed right click." );
         }
 
         if ( Input.GetMouseButtonDown( 2 ) )
@@ -38,7 +55,7 @@ public class Combat : MonoBehaviour
 
     void LeftClickCombo ()
     {
-        if( !isComboClick() )
+        if ( !IsComboClick() )
         {
             multClickCountLeft = 0;
             multClickCountRight = 0;
@@ -48,25 +65,36 @@ public class Combat : MonoBehaviour
         {
             case 0:
                 // Perform basic move 1 left click
+                ShowComboText ( "LEFT 1!" );
                 break;
             case 1:
                 // Perform basic move 2 left click
+                ShowComboText ( "LEFT 2!" );
                 break;
             case 2:
                 // Perform basic move 3 left click
+                ShowComboText ( "LEFT 3!" );
                 break;
             case 3:
                 // Perform basic move 4 left click
+                ShowComboText ( "LEFT 4!" );
                 break;
             default:
+                // Reset to zero if over 4
+                multClickCountLeft = 0;
+                multClickCountRight = 0;
+
+                // Perform basic move 1 left click
+                ShowComboText ( "LEFT 1!" );
                 break;
-        }
+        } 
+        multClickCountRight = 0;
         multClickCountLeft++;
     }
 
     void RightClickCombo ()
     {
-        if ( !isComboClick() )
+        if ( !IsComboClick() )
         {
             multClickCountLeft = 0;
             multClickCountRight = 0;
@@ -75,25 +103,50 @@ public class Combat : MonoBehaviour
         switch ( multClickCountLeft )
         {
             case 0:
+                // Perform smash move 0 right click
+                ShowComboText( "RIGHT 0!");
+                break;
+            case 1:
                 // Perform smash move 1 right click
-                break;                  
-            case 1:                     
+                ShowComboText ( "RIGHT 1!" );
+                break;
+            case 2:
                 // Perform smash move 2 right click
-                break;                  
-            case 2:                     
+                ShowComboText ( "RIGHT 2!" );
+                break;
+            case 3:
                 // Perform smash move 3 right click
-                break;                  
-            case 3:                     
+                ShowComboText ( "RIGHT 3!" );
+                break;
+            case 4:
                 // Perform smash move 4 right click
+                ShowComboText ( "RIGHT 4!" );
                 break;
             default:
+                // Reset to zero if over 4
+                multClickCountLeft = 0;
+                multClickCountRight = 0;
                 break;
         }
+        multClickCountLeft = 0;
         multClickCountRight++;
     }
 
-    bool isComboClick ()
+    bool IsComboClick ()
     {
-        return ( multClickTimer - Time.time ) <= delayBetweenMultClicks;
+        if ( ( clickTimer - prevClickTimer ) <= delayBetweenMultClicks )
+        {
+            prevClickTimer = clickTimer;
+            return true;
+        }
+        prevClickTimer = clickTimer;
+        return false;
+    }
+
+    void ShowComboText( string text )
+    {
+        fadeTimer = 0.75f;
+        m_ComboTextObj.SetActive( true );
+        m_ComboText.text = text;
     }
 }
