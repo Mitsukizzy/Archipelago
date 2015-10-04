27ec0656b4833b3441c7a3192c2083b83b21cd07
Jasmine Ying <jasminay@usc.edu>
2015-10-03 22:19:10 -0700

    Working on inventory UI

---
+++ Assets/__Scripts/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+    public GameObject slots;
+    int initSlotPosx = 40;
+    int initSlotPosy = 40;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 1; i < 8; i++)
+        {
+            GameObject slot = (GameObject)Instantiate(slots);
+            slot.transform.parent = this.gameObject.transform;
+            slot.name = "Slot " + i; 
+            slot.GetComponent<RectTransform>().localPosition = new Vector3(initSlotPosx, initSlotPosy, 0);
+            initSlotPosx += 70;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
