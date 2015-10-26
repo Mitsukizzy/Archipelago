85593d5666e203e79b35dcca5feeabe68f29f3d1
Jasmine Ying <jasminay@usc.edu>
2015-10-26 14:04:07 -0700

    Arrow shooting fixed

--- Assets/__Scripts/Arrow.cs
+++ Assets/__Scripts/Arrow.cs
@@ -4,18 +4,32 @@ using System.Collections;
 public class Arrow : MonoBehaviour
 {
     Vector3 pos;
-    float speed = 5f;
-    public float dist = 10f;
+	Vector3 dir;
+    public float speed = 5f;
     // Use this for initialization
     void Start ()
     {
-        pos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+		pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		pos.z = transform.position.z;
 
+		dir = pos - transform.position;
+
+		if( dir != Vector3.zero )
+		{
+			dir.Normalize ();
+		}
+
+		Quaternion rotateTo = new Quaternion();
+		rotateTo.SetFromToRotation(Vector3.right, dir);
+		transform.rotation = rotateTo;
     }
 
-    // Update is called once per frame
     void Update ()
     {
-        Vector3.MoveTowards(transform.position, pos, speed*Time.deltaTime);
-    }
+		transform.position += dir * 50 * Time.deltaTime;
+		if(transform.position.x > (Camera.main.orthographicSize * Camera.main.aspect) && 
+		   transform.position.y > (Camera.main.orthographicSize) ){
+				Destroy (gameObject);
+		}
+	}
 }
