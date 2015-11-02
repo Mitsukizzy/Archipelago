595e09c0b7ab20e67f0a6ac244862e0682832810
Jasmine Ying <jasminay@usc.edu>
2015-11-01 20:44:22 -0800

    Fixed birds not attacking

--- Assets/__Scripts/BirdAI.cs
+++ Assets/__Scripts/BirdAI.cs
@@ -159,7 +159,6 @@ public class BirdAI : MonoBehaviour {
         nextWaypoint = m_Waypoints[(Random.Range(0, m_Waypoints.Length))];
         moveTime = Time.time;
         journeyLength = Vector3.Distance(curWaypoint, nextWaypoint);
-        canAttack = false;
         isSafe = true;
     }
     public void exitedSafeArea()
