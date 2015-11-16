344c737c43d5ca624c37469475f88438d5c3cbfd
Jasmine Ying <jasminay@usc.edu>
2015-11-16 03:42:02 -0800

    Cheat keys disabled for beta build

--- Assets/__Scripts/GameManager.cs
+++ Assets/__Scripts/GameManager.cs
@@ -255,16 +255,16 @@ public class GameManager : MonoBehaviour
         // Our cheat reset keys
         if ( m_input.ResetGameButtonPressed () )
         {
-            Application.LoadLevel ( 0 ); // back to main menu
+            //Application.LoadLevel ( 0 ); // back to main menu
         }
         if ( m_input.ResetFromCampButtonPressed () )
         {
             // TODO (this will just load wetlands atm)
-            Application.LoadLevel ( 2 ); // back to latest campfire save
+            //Application.LoadLevel ( 2 ); // back to latest campfire save
         } 
         if ( m_input.NextLevelButtonPressed () )
         {
-            LoadNextLevel ();
+            //LoadNextLevel ();
         }
 	}
 
